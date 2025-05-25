b2fe12831efd0cef4d553a6d4068429570e3e74c
PhamTatThanh-VNU <thanhpham2k4nz@gmail.com>
2025-05-25 15:07:09 +0700

    update build docker

--- buildimage.sh
+++ buildimage.sh
@@ -10,4 +10,5 @@ docker build \
   --build-arg VITE_DOCUMENTDB_URI="$VITE_DOCUMENTDB_URI" \
   --build-arg VITE_DOCUMENTDB_DBNAME=$VITE_DOCUMENTDB_DBNAME \
   --build-arg VITE_BACKEND_URL=$VITE_BACKEND_URL \
+  --build-arg VITE_GOOGLE_GEMINI_API_KEY=$VITE_GOOGLE_GEMINI_API_KEY \
   -t educonnect-app .
